d4c67d91afa8e1ff1330d5f7770f79df42f9de48
pdoulatshahi <paul.doulatshahi@gmail.com>
2017-06-12 22:14:04 -0700

    Hashing user PW before storing

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -8,21 +8,42 @@ defmodule Calcutta.UserController do
     render(conn, "index.html", users: users)
   end
 
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(User, id)
+    changeset = User.changeset(user)
+    render(conn, "show.html", user: user, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Repo.get(User, id)
+    changeset = User.registration_changeset(user, user_params)
+    case Repo.update(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User updated")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> render("show.html", user: user, changeset: changeset)
+    end
+  end
+
   def new(conn, _params) do
     changeset = User.changeset(%User{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"user" => user_params}) do
-  changeset = User.changeset(%User{}, user_params)
-  case Repo.insert(changeset) do
-    {:ok, user} ->
-      conn
-      |> put_flash(:info, "User created!")
-      |> redirect(to: user_path(conn, :index))
-    {:error, changeset} ->
-      conn
-      |> render("new.html", changeset: changeset)
+    changeset = User.registration_changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> render("new.html", changeset: changeset)
+    end
   end
-end
+
 end
